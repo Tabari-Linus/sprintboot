fa4b2691c230de3a5c2134fc18be3bf79f0c98dc
Mr Lii <leetamparega@gmail.com>
2025-05-25 23:38:54 +0000

    moved to sub folder

---
+++ photoz-clone/src/main/java/lii/photozclone/Photos.java
@@ -0,0 +1,32 @@
+package lii.photozclone;
+
+public class Photos {
+
+    private Integer id;
+
+    private String fileName;
+
+    public Photos() {
+    }
+
+    public Photos(Integer id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
--- photoz-clone/src/main/java/lii/photozclone/PhotozController.java
+++ photoz-clone/src/main/java/lii/photozclone/PhotozController.java
@@ -3,12 +3,22 @@ package lii.photozclone;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class PhotozController {
+
+    List<Photos> photos = List.of(new Photos(1, "Hello.jpg"));
 
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/photoz")
+    public List<Photos> photoz() {
+        return
     }
 }
 
