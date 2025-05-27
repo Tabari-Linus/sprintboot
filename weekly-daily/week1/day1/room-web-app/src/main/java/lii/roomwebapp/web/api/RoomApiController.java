f1a01906d805e61014c6916131306360069c3375
Mr Lii <leetamparega@gmail.com>
2025-05-27 10:32:49 +0000

    Create RoomAPiController and add methods to us service

--- weekly-daily/week1/day1/room-web-app/src/main/java/lii/roomwebapp/web/api/RoomApiController.java
+++ weekly-daily/week1/day1/room-web-app/src/main/java/lii/roomwebapp/web/api/RoomApiController.java
@@ -1,4 +1,47 @@
 package lii.roomwebapp.web.api;
 
+import lii.roomwebapp.service.RoomService;
+import lii.roomwebapp.web.model.Room;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomApiController {
+
+    private final RoomService roomService;
+
+    public RoomApiController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return roomService.getAllRooms();
+    }
+
+    @GetMapping("/{id}")
+    public Room getRoomById(@PathVariable(name = "id") UUID id) {
+        return roomService.getRoomById(id);
+    }
+
+    @PostMapping
+    public Room createRoom(@RequestBody Room room) {
+        return roomService.createRoom(room);
+    }
+
+    @PutMapping("/{id}")
+    public Room updateRoom(@PathVariable(name = "id") UUID id, @RequestBody Room room) {
+        room.setId(id);
+        return roomService.updateRoom(room);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRoom(@PathVariable(name = "id") UUID id) {
+        roomService.deleteRoomById(id);
+    }
+
+
 }
