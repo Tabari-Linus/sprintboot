1774d5c0977372d614fe998852a95f04fd11f178
Mr Lii <leetamparega@gmail.com>
2025-06-02 11:40:39 +0000

    docs: add pageable for custom query

--- springdata/src/main/java/lii/springdata/repository/StudentRepository.java
+++ springdata/src/main/java/lii/springdata/repository/StudentRepository.java
@@ -1,6 +1,8 @@
 package lii.springdata.repository;
 
 import lii.springdata.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,6 +12,10 @@ import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByName(String name);
+
+    Page<Student> findByNameContaining(String name, Pageable pageable);
+
+    Page<Student> findAll(Pageable pageable);
 
     List<Student> findByAgeBetween(int min, int max);
 
