6a85ee9a6cd32d64af36def4755a7b239d0f3c03
Mr Lii <leetamparega@gmail.com>
2025-06-09 15:53:57 +0000

    config: using bycrypt for hashing passwords

--- springsecurity/spring-6-spring-security/admin-web/src/main/resources/data.sql
+++ springsecurity/spring-6-spring-security/admin-web/src/main/resources/data.sql
@@ -10,8 +10,8 @@ INSERT INTO orders (order_id, customer_id, order_info) values (RANDOM_UUID(), (S
 INSERT INTO orders (order_id, customer_id, order_info) values (RANDOM_UUID(), (SELECT customer_id FROM customers where name = 'Acme'), '3000 Widgets');
 INSERT INTO orders (order_id, customer_id, order_info) values (RANDOM_UUID(), (SELECT customer_id FROM customers where name = 'Callahan Auto'), '200 Widgets');
 
-INSERT INTO users (username, password, enabled) values  ('user', 'passwordjdbc', true);
-INSERT INTO users (username, password, enabled) values  ('admin', 'passwordjdbc', true);
+INSERT INTO users (username, password, enabled) values  ('user', '{bcrypt}$2a$10$fpf1oIOBIH2c.k4HwBqbLe4EnXeL9il7b6rnUB9CWdE8MNp3yQyfK', true);
+INSERT INTO users (username, password, enabled) values  ('admin', '{bcrypt}$2a$10$fpf1oIOBIH2c.k4HwBqbLe4EnXeL9il7b6rnUB9CWdE8MNp3yQyfK', true);
 
 INSERT INTO authorities (username, authority) values ('user', 'ROLE_USER');
 INSERT INTO authorities (username, authority) values ('admin', 'ROLE_USER');
